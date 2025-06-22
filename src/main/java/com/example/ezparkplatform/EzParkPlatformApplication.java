f63e4466ab30694e993e544de4b510f15148d3c6
Andres-0304 <andresfer0304@gmail.com>
2025-06-22 10:38:54 -0500

    chore: initial commit

---
+++ src/main/java/com/example/ezparkplatform/EzParkPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.ezparkplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzParkPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EzParkPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ezparkplatform/EzParkPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ezparkplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzParkPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
